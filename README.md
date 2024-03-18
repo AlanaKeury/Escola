07ba3901d97fbf04cd01edc4b6ec8a7ba95eee30
Alana Keury <162646307+AlanaKeury@users.noreply.github.com>
2024-03-18 17:36:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Escola
+ atividade de Jeff
---
+++ models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Professor(models.Model):
+    nome= models.CharField(max_length=150)
+    email= models.EmailField()
+    celular= models.CharField(max_length=20)
+    def __str__(self):
+        return self.nome
+
+class Disciplina(models.Model):
+    nome= models.CharField(max_length=150)
+    carga_horaria= models.IntegerField()
+    codigo= models.CharField(max_length=150)
+    def __str__(self):
+        return self.nome
